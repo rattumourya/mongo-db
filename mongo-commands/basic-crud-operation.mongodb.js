6d9d2b66c78647ae2039f40c691153ba68430d3a
Ratan Mourya <68645660+ratanlalmourya@users.noreply.github.com>
2024-02-02 12:53:46 +0000

    flight db created and flightData collection created with two objects

--- mongo-commands/basic-crud-operation.mongodb.js
+++ mongo-commands/basic-crud-operation.mongodb.js
@@ -1,5 +1,17 @@
+// show all available db
+show('dbs');
+
 // switch to flight database
 use("flights");
 
+// insert document collection 
+// db.flightData.insert({
+//     "departureAirport": "LHR",
+//     "arrivalAirport": "TXL",
+//     "aircraft": "Airbus A320",
+//     "distance": 950,
+//     "intercontinental": false
+//   })
+
 // get flightData collection data
-db.flightData.find()
+db.flightData.find().pretty();
