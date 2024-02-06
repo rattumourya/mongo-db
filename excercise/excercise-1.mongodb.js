9bd55fa018f1e62c719ede607330ecfd7f8e29da
Ratan Mourya <68645660+ratanlalmourya@users.noreply.github.com>
2024-02-06 16:47:26 +0000

    Schema validation added on posts

--- excercise/excercise-1.mongodb.js
+++ excercise/excercise-1.mongodb.js
@@ -1,4 +1,4 @@
-show("dbs"); 
+show("dbs");
 // users , comments , posts
 // create blog database
 use("blog");
@@ -22,4 +22,110 @@ use("blog");
 //     ]
 // });
 // see our post in the playground
+// db.posts.find().pretty();
+
+// ******************************** Schema Validation ************************
+// db.posts.drop();
+// db.posts.find().pretty();
+// db.createCollection("posts", {
+//     validator: {
+//         $jsonSchema: {
+//             bsonType: "object",
+//             required: ["title", "text", "creator", "comments"],
+//             properties: {
+//                 title:{
+//                     bsonType:"string",
+//                     description:"must be a string and is required"
+//                 },
+//                 text: {
+//                     bsonType: "string",
+//                     description:"must be a string and is required"
+//                 },
+//                 creator:{
+//                     bsonType:"objectId",
+//                     description:"must be an objectid and is required"
+//                 },
+//                 comments: {
+//                     bsonType:"array",
+//                     description:"must be a array and is required",
+//                     items:{
+//                         bsonType:"object",
+//                         required: ["text","author"],
+//                         properties: {
+//                             text:{
+//                                 bsonType:"string",
+//                                 description: "must be a string and is required"
+//                             },
+//                             author:{
+//                                 bsonType:"objectId",
+//                                 description:"must be an objectid and is required"
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// });
+
+// db.posts.insertOne({
+//     title:"My First Post",
+//     text: "This is my first post, I hope you liked it!",
+//     tags:["new","tech"],
+//     creator: ObjectId("65c23f501c4f1be97f1ad0e0"),
+//     comments: [
+//         {text: "I liked this post",author: ObjectId("65c23f501c4f1be97f1ad0e1")}
+//     ]
+// });
+// db.posts.find().pretty();
+// db.runCommand({collMod:"posts",  validator: {
+//     $jsonSchema: {
+//         bsonType: "object",
+//         required: ["title", "text", "creator", "comments"],
+//         properties: {
+//             title:{
+//                 bsonType:"string",
+//                 description:"must be a string and is required"
+//             },
+//             text: {
+//                 bsonType: "string",
+//                 description:"must be a string and is required"
+//             },
+//             creator:{
+//                 bsonType:"objectId",
+//                 description:"must be an objectid and is required"
+//             },
+//             comments: {
+//                 bsonType:"array",
+//                 description:"must be a array and is required",
+//                 items:{
+//                     bsonType:"object",
+//                     required: ["text","author"],
+//                     properties: {
+//                         text:{
+//                             bsonType:"string",
+//                             description: "must be a string and is required"
+//                         },
+//                         author:{
+//                             bsonType:"objectId",
+//                             description:"must be an objectid and is required"
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// },
+// validationAction:"warn"
+// });
+
+// db.posts.insertOne({
+//     title:"My First Post",
+//     text: "This is my first post, I hope you liked it!",
+//     tags:["new","tech"],
+//     creator: ObjectId("65c23f501c4f1be97f1ad0e0"),
+//     comments: [
+//         {text: "I liked this post",author: 12}
+//     ]
+// });
 db.posts.find().pretty();
