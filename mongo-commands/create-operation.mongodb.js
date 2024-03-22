33788873f8a0bf1b249e1870067c6eca78fec59b
mouryatutor <156491972+mouryatutor@users.noreply.github.com>
2024-03-22 16:02:21 +0000

    Creating document , insert and ordered insert

---
+++ mongo-commands/create-operation.mongodb.js
@@ -0,0 +1,63 @@
+// show('dbs');
+
+// use("test");
+// db.dropDatabase();
+// show('dbs');
+
+use("contactData");
+// db.personData.insertOne({
+//     name:"Max",
+//     age: 30,
+//     hobbies: ["Sports","Cooking"]
+// });
+
+// db.personData.insertOne({
+//     name:"Manuel",
+//     age: 31,
+//     hobbies: ["Cars","Cooking"]
+// });
+
+// db.personData.insertMany([{
+//     name:"Anna",
+//     age: 29,
+//     hobbies:["Sports","Yoga"]
+// }]);
+
+
+// db.personData.insertMany([
+//     {name: "Maria",age:31},
+//     {name: "Chris",age:25}
+// ]);
+
+// insert method not recommended
+// db.personData.insert({
+//     name:"Phil",
+//     age:35
+// })
+
+// show("dbs");
+
+// db.personData.insert([
+//     {name: "Sandeep",age:28},
+//     {name: "Hans",age:38}
+// ]);
+
+// db.personData.find();
+
+
+// db.hobbies.insertMany([
+//     {_id: "sports", name: "Sports"},
+//     {_id: "cooking", name: "Cooking"},
+//     {_id: "cars", name: "Cars"}
+// ]); 
+
+// db.hobbies.insertMany([
+//     {_id: "yoga", name: "Yoga"},
+//     {_id: "cooking", name: "Cooking"},
+//     {_id: "hiking", name: "Hiking"}],
+//     {ordered:false}); 
+
+// E11000 duplicate key error collection: contactData.hobbies index: _id_ dup key: { _id: "cooking" }
+
+
+db.hobbies.find().pretty();
