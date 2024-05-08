714914110a304b48263bddd22719216b9d78531f
rattumourya <160098742+rattumourya@users.noreply.github.com>
2024-05-08 15:49:23 +0000

    $and operators

--- mongo-commands/read-operations.mongodb.js
+++ mongo-commands/read-operations.mongodb.js
@@ -32,4 +32,8 @@ use("shows");
 // db.movies.find({"rating.average": {$gt: 9.3}}).count();
 
 // db.movies.find({$or: [{"rating.average": {$lt: 5}}, {"rating.average": {$gt: 9.3}}]}).pretty();
-db.movies.find({$nor: [{"rating.average": {$lt: 5}}, {"rating.average": {$gt: 9.3}}]}).pretty();
+// db.movies.find({$nor: [{"rating.average": {$lt: 5}}, {"rating.average": {$gt: 9.3}}]}).pretty();
+
+// db.movies.find({$and: [{"rating.average": {$gt: 9}},{genres: "Drama"}]}).pretty();
+// db.movies.find({"rating.average": {$gt: 9}, "genres": "Drama"});
+db.movies.find({"rating.average": {$gt: 9}, "genres": "Drama"}).count();
