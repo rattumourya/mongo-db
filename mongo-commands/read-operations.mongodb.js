f77143eaa0ac1afcccea847fb86b485ddd5327be
rattumourya <160098742+rattumourya@users.noreply.github.com>
2024-05-08 15:01:20 +0000

    Understanding fineOne() and find()

--- mongo-commands/read-operations.mongodb.js
+++ mongo-commands/read-operations.mongodb.js
@@ -1,2 +1,12 @@
 // mongoimport --db shows --collection movies --file json_files/tv-shows.json --jsonAr
 // ray
+
+use("shows");
+// db.movies.findOne();
+
+// db.movies.find({name:"The Last Ship"}).pretty();
+
+// db.movies.find({runtime: 60}).pretty();
+// db.movies.findOne({runtime: 60});
+
+
